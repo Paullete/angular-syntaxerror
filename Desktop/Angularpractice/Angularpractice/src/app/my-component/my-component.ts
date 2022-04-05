d11338c14c98cab2e9787a0c033945892b184e90
paullete <paulettenereah3@gmail.com>
2022-04-05 17:56:35 +0300

    initial commit

---
+++ Desktop/Angularpractice/Angularpractice/src/app/my-component/my-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-component',
+  templateUrl: './my-component.html',
+  styleUrls: ['./my-component.css']
+})
+export class AppComponent {
+  title = 'Angularpractice';
+}
