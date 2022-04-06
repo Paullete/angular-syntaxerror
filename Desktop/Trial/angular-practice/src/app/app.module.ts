2868edf80b2e12b33e4bf7ea699f107be7d2441b
paullete <paulettenereah3@gmail.com>
2022-04-06 18:55:42 +0300

    cli component

--- Desktop/Trial/angular-practice/src/app/app.module.ts
+++ Desktop/Trial/angular-practice/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {MyFirstComponent} from "./my-component/my-component"
+import {MyFirstComponent} from "./my-component/my-component";
+import { MySecondComponentComponent } from './my-second-component/my-second-component.component'
 @NgModule({
   declarations: [
-    AppComponent, MyFirstComponent
+    AppComponent, MyFirstComponent, MySecondComponentComponent
   ],
   imports: [
     BrowserModule
