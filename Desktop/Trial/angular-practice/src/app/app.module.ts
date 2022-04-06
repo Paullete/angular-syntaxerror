2289d58ab9d3cc72371620f634463b41f40ea72a
paullete <paulettenereah3@gmail.com>
2022-04-06 18:25:33 +0300

    first component

--- Desktop/Trial/angular-practice/src/app/app.module.ts
+++ Desktop/Trial/angular-practice/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import {MyFirstComponent} from "./my-component/my-component"
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MyFirstComponent
   ],
   imports: [
     BrowserModule
---
+++ Desktop/Trial/angular-practice/src/app/my-component/my-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-first-component',
+  templateUrl: './my-component.html',
+  styleUrls: ['./my-component.css']
+})
+export class MyFirstComponent {
+  title = 'angular-practice';
+}
