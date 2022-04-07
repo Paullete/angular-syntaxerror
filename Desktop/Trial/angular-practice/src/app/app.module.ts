3dbb2f4d6f9b38086b2f6ca41ab79618272821e8
paullete <paulettenereah3@gmail.com>
2022-04-07 22:51:09 +0300

    pipengoals

--- Desktop/Trial/angular-practice/src/app/app.module.ts
+++ Desktop/Trial/angular-practice/src/app/app.module.ts
@@ -7,10 +7,12 @@ import {MyFirstComponent} from "./my-component/my-component";
 import { MySecondComponentComponent } from './my-second-component/my-second-component.component'
 import TheInterpolationComponent from './basics/databinding/interpolationcomponet/interpolation-component';
 import { PropertyBindingComponent } from './basics/databinding/property-binding/property-binding.component';
-import { EventBindingComponent } from './basics/databinding/event-binding/event-binding.component'
+import { EventBindingComponent } from './basics/databinding/event-binding/event-binding.component';
+import { GoalComponent } from './goal/goal/goal.component';
+
 @NgModule({
   declarations: [
-    AppComponent, MyFirstComponent, MySecondComponentComponent, TheInterpolationComponent, PropertyBindingComponent, EventBindingComponent
+    AppComponent, MyFirstComponent, MySecondComponentComponent, TheInterpolationComponent, PropertyBindingComponent, EventBindingComponent, GoalComponent, 
   ],
   imports: [
     BrowserModule
