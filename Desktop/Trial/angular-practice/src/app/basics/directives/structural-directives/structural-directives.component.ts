455895dd4136ea444ba7577f4e03589cb78355c6
paullete <paulettenereah3@gmail.com>
2022-04-11 13:53:40 +0300

    details

--- Desktop/Trial/angular-practice/src/app/basics/directives/structural-directives/structural-directives.component.ts
+++ Desktop/Trial/angular-practice/src/app/basics/directives/structural-directives/structural-directives.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./structural-directives.component.css']
 })
 export class StructuralDirectivesComponent implements OnInit {
-
+showElement= true
   constructor() { }
 
   ngOnInit(): void {
+  }
+  toogleElement(){
+    this.showElement=this.showElement
   }
 
 }
