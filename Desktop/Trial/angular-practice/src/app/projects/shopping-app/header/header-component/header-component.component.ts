6c6dd6ac5afd6c59d8ab94d10d630385ce8aede7
paullete <paulettenereah3@gmail.com>
2022-04-12 15:40:05 +0300

    details

--- Desktop/Trial/angular-practice/src/app/projects/shopping-app/header/header-component/header-component.component.ts
+++ Desktop/Trial/angular-practice/src/app/projects/shopping-app/header/header-component/header-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header-component',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-component.component.css']
 })
 export class HeaderComponentComponent implements OnInit {
+  @Input() logo = 'Shoping List App';
+  @Input() btn1 = 'New Item';
+  @Input() btn2 = 'Card';
+  @Output() btnPress = new EventEmitter();
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  btnPressEvent = (str: string) => {
+    this.btnPress.emit(str);
+  };
 }
